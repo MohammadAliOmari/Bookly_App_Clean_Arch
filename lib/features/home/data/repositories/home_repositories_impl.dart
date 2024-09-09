89cdbe6df9817fe8296a4670708828f11bdf1113
Mohamad Omari <mohamadomari.2546@gmail.com>
2024-09-10 00:45:43 +0300

    - create server faulire from Dio Exception

--- lib/features/home/data/repositories/home_repositories_impl.dart
+++ lib/features/home/data/repositories/home_repositories_impl.dart
@@ -4,6 +4,7 @@ import 'package:bookly/features/home/data/data_source/home_remote_data_source.da
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/repositories/home_repository.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepositoriesImpl extends HomeRepository {
   final HomeRemoteDataSource homeRemoteDataSource;
@@ -21,7 +22,11 @@ class HomeRepositoriesImpl extends HomeRepository {
       books = await homeRemoteDataSource.fetchFeaturedBooks();
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
     }
   }
 
@@ -36,7 +41,11 @@ class HomeRepositoriesImpl extends HomeRepository {
       books = await homeRemoteDataSource.fetchNewestBooks();
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
     }
   }
 }
